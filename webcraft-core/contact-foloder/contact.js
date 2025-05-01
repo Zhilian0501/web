99c9f8d1cb1062947e3f5ee5dffb7695776821df
Zhilian0501 <zhilianshuwei@gmail.com>
2025-05-01 17:17:50 +0000

    Changes to be committed: 	renamed:    member-core/member-foloder/salary-foloder/salary.css -> admin-core/admin-foloder/admin.css 	renamed:    member-core/member-foloder/salary-foloder/salary.html -> admin-core/admin-foloder/admin.html 	renamed:    member-core/member-foloder/salary-foloder/salary.js -> admin-core/admin-foloder/admin.js 	new file:   admin-core/calender-foloder/calender.css 	new file:   admin-core/calender-foloder/calender.html 	new file:   admin-core/calender-foloder/calender.js 	new file:   admin-core/docs-foloder/docs.css 	new file:   admin-core/docs-foloder/docs.html 	new file:   admin-core/docs-foloder/docs.js 	new file:   admin-core/taskes-folofer/taskes.css     new file:   admin-core/taskes-folofer/taskes.html 	new file:   admin-core/taskes-folofer/taskes.js 	new file:   admin-core/users-foloder/users.css 	new file:   admin-core/users-foloder/users.html 	new file:   admin-core/users-foloder/users.js 	new file:   member-core/forms-foloder/forms.css 	new file:   member-core/forms-foloder/forms.html 	new file:   member-core/forms-foloder/forms.js 	modified:   member-core/member-foloder/member.html 	new file:   member-core/salary-foloder/salary.css 	new file:   member-core/salary-foloder/salary.html 	new file:   member-core/salary-foloder/salary.js 	modified:   webcraft-core/about-foloder/about.css 	modified:   webcraft-core/about-foloder/about.html 	modified:   webcraft-core/about-foloder/about.js 	modified:   webcraft-core/contact-foloder/contact.css 	modified:   webcraft-core/contact-foloder/contact.html 	modified:   webcraft-core/contact-foloder/contact.js 	modified:   webcraft-core/index-foloder/index.css 	modified:   webcraft-core/index-foloder/index.html 	modified:   webcraft-core/index-foloder/index.js 	modified:   webcraft-core/portfolio-foloder/portfolio.css 	modified:   webcraft-core/portfolio-foloder/portfolio.html 	modified:   webcraft-core/portfolio-foloder/portfolio.js 	modified:   webcraft-core/services-foloder/services.html 	modified:   webcraft-core/services-foloder/servies.css 	modified:   webcraft-core/services-foloder/servies.js

---
+++ webcraft-core/contact-foloder/contact.js
@@ -0,0 +1,10 @@
+// 漢堡選單功能
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+  
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+    });
+  });
+  
